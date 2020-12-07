66e684ffde70cfd96e92f4a24f00b854bb41dfbf
Rbloemhard <raymond_bloemhard@hotmail.com>
2020-12-07 14:28:37 +0100

    added code

---
+++ level14-opdracht4/arraymethods.js
@@ -0,0 +1,13 @@
+//while loop
+let colors = ['yellow','blue','red','orange'];
+let i = 0;
+while(i < colors.length){
+    console.log(colors[i]);
+    i++;
+}
+
+//for loop
+
+for(i = 0; i < colors.length; i++){
+    console.log(colors[i]);
+}
