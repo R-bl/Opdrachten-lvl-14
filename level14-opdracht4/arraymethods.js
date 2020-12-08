e5589bc75e9e1981d97409c155e40eec96ad1fbe
Rbloemhard <raymond_bloemhard@hotmail.com>
2020-12-08 13:45:45 +0100

    added for in loop

--- level14-opdracht4/arraymethods.js
+++ level14-opdracht4/arraymethods.js
@@ -17,7 +17,27 @@ for(i = 0; i < colors.length; i++){
 
 
 // foreach method 1 regel
+
+
+
 colors.forEach(element => console.log(element));
-/* is hetzelfde als
-color.forEach(function(element){
-    console.log(element))*/
+
+
+/* wat ik fijn vindt aan de foreach method is
+   dat het kort geschreven kan worden, en dat er voor elk element in de array eigenlijk
+   een eigen functie wordt gemaakt dat gelijk een waarde heeft, en een index
+   */
+
+
+
+let myObject = {name: 'Raymond', age: 30, city: 'Deventer',Music: 'House',Food: 'ClubSandwich'};
+
+for(i in myObject){
+    console.log(`${i} : ${myObject[i]}`);
+}
+/*Hier maak ik gebruik van de for in method.
+  i is hier de property van myObject*/
+
+   
+   
+
