28ead961611dcb4be6d8986fd8588b906bb5651f
Rbloemhard <raymond_bloemhard@hotmail.com>
2020-12-09 18:38:18 +0100

    test

---
+++ level14-dicegame/game.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+playerNames: function(player_nr, msg = "Enter your name") {
+    let player_name = window.prompt(`${msg} Player${player_nr}`);
+
+    if (player_name === null || player_name === "")
+        playerNames(player_nr, "Please enter a valid name");
+
+    return player_name;
+}
+*/
