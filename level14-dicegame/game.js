f7b52affe321835c80762481a91e9f40fd2a0f13
Rbloemhard <raymond_bloemhard@hotmail.com>
2020-12-10 13:40:32 +0100

    PLayer prompt

--- level14-dicegame/game.js
+++ level14-dicegame/game.js
@@ -1,32 +1,22 @@
+//Welkom bericht en Vraag om naam in te voeren
 
 
+let player = function(number, msg = 'Welkom! wat is je naam?'){
+    let player_name = prompt(`${msg}`);
+    
+    if(player_name === null || player_name === "")
+        player('Voer geldige naam in.');
+        
+    alert(`Hey ${player_name}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-playerNames: function(player_nr, msg = "Enter your name") {
-    let player_name = window.prompt(`${msg} Player${player_nr}`);
-
-    if (player_name === null || player_name === "")
-        playerNames(player_nr, "Please enter a valid name");
-
-    return player_name;
 }
-*/
+let getRandomNr = function(start, range){
+    let randomNumber = Math.floor(Math.random() * range) + 1;
+    
+}
+
+
+player();
+
+
+l
